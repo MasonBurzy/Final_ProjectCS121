8398a804af2c6c52122f3200188654d356ee5fb6
MasonBurzy <mason.burzynski@bsu.edu>
2025-04-16 21:09:15 +0000

    updated Car & User

---
+++ Car.java
@@ -0,0 +1,29 @@
+/// Car ///
+
+public abstract class Car {
+
+	private String carID;
+	private boolean isRented;
+
+	public String getCarID() {
+
+	}
+
+	public boolean is REnted() {
+	
+	}
+
+	public void rent() {
+	
+	}
+
+	public void returnCar() {
+	
+	}
+
+	public String toString() {
+	
+	}
+
+
+
--- User.java
+++ User.java
@@ -1,5 +1,6 @@
 /// User ///
 
+import java.io.*
 
 public abstract class User implements HasMenu {
 
@@ -7,23 +8,40 @@ public abstract class User implements HasMenu {
 	private String PIN = "";
 
 	public boolean login(userName, PIN) {
-	
-	}
+		scanner input = new Scanner(system.in);
+		boolean result = false;
+		System.out.println("User name: ");
+		String userName = input.nextLine();
 
-	public void setUserName(userName) {
-	
+		if (userNameIn.equals(this.userName)){
+			System.out.println("PIN: ");
+			String pinIN = input.nextLine();
+			if (pinIN.equals(this.PIN)) {
+				System.out.println("Login Successful");
+				result = true;
+			} else {
+				System.out.println("Invalid PIN");
+			} // end if
+		} else {
+			System.out.println("Invalid User Name");
+		} // end if
+		return result;
+	} // end Login 
+
+	public void setUserName(String userName) {
+		this.userName = userName;
 	}
 
 	public String getUserName() {
-	
+		return userName;
 	}
 
 	public void setPIN(PIN) {
-	
+		this.PIN = PIN;
 	}
 
 	public String getPIN() {
-	
+		return PIN;
 	}
 
 
